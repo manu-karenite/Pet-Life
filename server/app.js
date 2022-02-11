a8c7764eaf1bece338a38875a8b8fc25aa1789ae
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-11 19:05:18 +0530

    Connection to DB Estabilished

--- server/app.js
+++ server/app.js
@@ -15,6 +15,16 @@ app.use(cors());
 //Using Morgan to display requests :
 app.use(morgan("dev"));
 
+//connecting to database .. :
+const mongoConnectionString = process.env.MONGO_SRV.replace(
+  "<password>",
+  process.env.MONGO_PASSWORD
+);
+mongoose
+  .connect(mongoConnectionString)
+  .then((res) => console.log("Database is Connected!"))
+  .catch((err) => console.log(err));
+
 //Importing Routers :
 const { AuthHotelRouter } = require("./Routes/Hotel/Authentication.js");
 app.use("/api/v1", AuthHotelRouter);
