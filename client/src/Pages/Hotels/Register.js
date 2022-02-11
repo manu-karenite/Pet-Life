dcd352ece8359f0d938d308f50384218f545a686
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-11 18:53:05 +0530

    Created A Page for Hotels Regisration

--- client/src/Pages/Hotels/Register.js
+++ client/src/Pages/Hotels/Register.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Register = () => {
-  return <div>Register</div>;
+const RegisterHotel = () => {
+  return <div>RegisterHotel</div>;
 };
 
-export default Register;
+export default RegisterHotel;
