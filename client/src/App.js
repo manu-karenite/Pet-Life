362b04f1f8dafd986d2273ffc57c34a4e4fb1ef5
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-11 21:47:59 +0530

    Created Header for Hotels Section

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 //importing only Hotels Routes
+import HotelAppBar from "./Components/AppBar/HotelAppBar.js";
 import RegisterHotel from "./Pages/Hotels/Register.js";
 function App() {
   return (
-    <Routes>
-      {/*Routes for Hotels Only*/}
-      <Route path="/hotel/register" exact element={<RegisterHotel />} />
-    </Routes>
+    <>
+      <HotelAppBar />
+      <Routes>
+        {/*Routes for Hotels Only*/}
+        <Route path="/hotel/register" exact element={<RegisterHotel />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/Components/AppBar/HotelAppBar.js
@@ -0,0 +1,152 @@
+import * as React from "react";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import Button from "@mui/material/Button";
+// import IconButton from "@mui/material/IconButton";
+// import MenuIcon from "@mui/icons-material/Menu";
+
+// import Menu from "@mui/material/Menu";
+import { Link } from "react-router-dom";
+// import MenuItem from "@mui/material/MenuItem";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import Divider from "@mui/material/Divider";
+import Settings from "@mui/icons-material/Settings";
+import { useState } from "react";
+import {
+  AppBar,
+  Tooltip,
+  Avatar,
+  Divider,
+  ListItemIcon,
+  MenuItem,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  MenuIcon,
+  Menu,
+} from "@mui/material";
+import { PersonAdd, Logout } from "@mui/icons-material";
+const HotelAppBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Hotels @ Pet-Life
+            </Typography>
+            {/* <Button color="inherit">Login</Button>
+          <Button color="inherit">SignUp</Button> */}
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">SignUp</Button>
+            <Tooltip title="Account settings">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? "account-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <Avatar
+                  sx={{
+                    width: 32,
+                    height: 32,
+                    backgroundColor: "#000",
+                    color: "white",
+                  }}
+                >
+                  N
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem>
+          <Avatar /> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default HotelAppBar;
