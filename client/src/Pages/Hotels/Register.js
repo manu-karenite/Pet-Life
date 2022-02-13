3d931e839792183d8aad68f5d2a2f6659ebff72a
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-13 11:08:15 +0530

    Created Register Form Component

--- client/src/Pages/Hotels/Register.js
+++ client/src/Pages/Hotels/Register.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import RegisterForm from "../../Components/Hotels/Forms/RegisterForm.js";
 const RegisterHotel = () => {
-  return <div>RegisterHotel</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row mt-3">
+        <div className="col-md-3" />
+        <div className="col-md-6">
+          <RegisterForm />
+          <div style={{ textAlign: "center" }} className="mt-3">
+            Already Registered? <Link to="/hotel/login">Login Now!</Link>
+          </div>
+        </div>
+        <div className="col-md-3" />
+      </div>
+    </div>
+  );
 };
 
 export default RegisterHotel;
