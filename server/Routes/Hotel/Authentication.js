d0772851ac703616d3dec8ec30fa83cbed821672
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-13 19:29:32 +0530

    Revamped the Route

--- server/Routes/Hotel/Authentication.js
+++ server/Routes/Hotel/Authentication.js
@@ -3,7 +3,7 @@ const AuthHotelRouter = express.Router();
 
 const { registerHotel } = require("../../Controllers/Hotel/Authentication.js");
 
-AuthHotelRouter.route("/hotel/register").post(registerHotel);
+AuthHotelRouter.route("/hotel/register-hotel").post(registerHotel);
 
 const object = { AuthHotelRouter };
 module.exports = object;
