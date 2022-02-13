99f526b9ab4f874a577a19f3493381a0326715ec
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-13 19:28:58 +0530

    Created API Endpoint for /hotel/register-hotel

--- server/Controllers/Hotel/Authentication.js
+++ server/Controllers/Hotel/Authentication.js
@@ -1,5 +1,20 @@
+const hotel = require("../../Models/Hotel.js");
 const registerHotel = async (req, res) => {
-  res.json("New Hotels");
+  let { name, email, contact, owner } = req.body;
+  try {
+    if (!name || !email || !contact || !owner) {
+      throw "Insufficient Details Found! Please Fill in the details Carefully";
+    }
+
+    //query the DB for any email present or not!
+    const checkEmail = await hotel.findOne({ email });
+    if (checkEmail) {
+      throw "Hotel Already Registered with Email Address! Please try a new One!";
+    }
+    res.status(200).json("ok");
+  } catch (error) {
+    return res.status(400).json(error);
+  }
 };
 
 const object = { registerHotel };
