0fcf8804a872657157a3cabf0be450363c2a041e
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-13 11:07:42 +0530

    Imported Hotel Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 //importing only Hotels Routes
 import HotelAppBar from "./Components/AppBar/HotelAppBar.js";
 import RegisterHotel from "./Pages/Hotels/Register.js";
+import HotelLogin from "./Pages/Hotels/Login.js";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         {/*Routes for Hotels Only*/}
         <Route path="/hotel/register" exact element={<RegisterHotel />} />
+        <Route path="/hotel/login" exact element={<HotelLogin />} />
       </Routes>
     </>
   );
