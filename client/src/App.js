729001362281c0532373fe65d0b9985f19314a7f
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-13 19:26:22 +0530

    Added ToastContainer for showing Toast Notifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
 //importing only Hotels Routes
 import HotelAppBar from "./Components/AppBar/HotelAppBar.js";
 import RegisterHotel from "./Pages/Hotels/Register.js";
@@ -8,6 +8,7 @@ import HotelLogin from "./Pages/Hotels/Login.js";
 function App() {
   return (
     <>
+      <ToastContainer />
       <HotelAppBar />
       <Routes>
         {/*Routes for Hotels Only*/}
