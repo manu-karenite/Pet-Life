3cc78b1a0547d7fcc268de1eab7a768f967fb714
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-17 20:45:18 +0530

    JWT Methos to signToken

--- server/Utitlities/JWTHandlers/signToken.js
+++ server/Utitlities/JWTHandlers/signToken.js
@@ -1,1 +1,13 @@
 const jwt = require("jsonwebtoken");
+
+const signToken = (parameter, payload) => {
+  try {
+    const result = jwt.sign({ parameter: payload }, process.env.JWT_SECRET, {
+      expiresIn: 900,
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = signToken;
