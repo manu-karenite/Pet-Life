19d8d6985c0135cab76ac9dbfcb5eea7373b3148
Alisha Anand <55910525+anandalisha@users.noreply.github.com>
2022-02-18 20:25:18 +0530

    Add files via upload

---
+++ pro/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar /> 
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={SignUp} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ pro/src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import ph1 from "../assets/ph1.jpg";
+import ph2 from "../assets/ph2.jfif";
+import ph3 from "../assets/ph3.jpg";
+import ph4 from "../assets/ph4.jpg";
+import ph5 from "../assets/ph5.jpg";
+import ph6 from "../assets/ph6.jfif";
+
+export const MenuList = [
+  {
+    name: "Oriana Pets' Hostel",
+    image: ph1,
+    price: 15.99,
+  },
+  {
+    name: "SANDF Pet Resort",
+    image: ph2,
+    price: 11.99,
+  },
+  {
+    name: "Kochi Pet Care",
+    image: ph3,
+    price: 256.53,
+  },
+  {
+    name: "Posh Paws Pets",
+    image: ph4,
+    price: 17.99,
+  },
+  {
+    name: "Royal Pet Hotel",
+    image: ph5,
+    price: 4.99,
+  },
+  {
+    name: "Petaholic Hill",
+    image: ph6,
+    price: 1997.99,
+  },
+];
