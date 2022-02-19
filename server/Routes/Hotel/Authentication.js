65b043a1afe54cc583555942f0067636b06c00e3
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-19 13:07:17 +0530

    Added Routes to register and confirmRegister a hotel

--- server/Routes/Hotel/Authentication.js
+++ server/Routes/Hotel/Authentication.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const AuthHotelRouter = express.Router();
 
-const { registerHotel } = require("../../Controllers/Hotel/Authentication.js");
+const {
+  registerHotel,
+  registerHotelConfirm,
+} = require("../../Controllers/Hotel/Authentication.js");
 
 AuthHotelRouter.route("/hotel/register-hotel").post(registerHotel);
-
+AuthHotelRouter.route("/hotel/register-hotel/confirm").post(
+  registerHotelConfirm
+);
 const object = { AuthHotelRouter };
 module.exports = object;
