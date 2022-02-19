7bf2245befb47a4a62f72d2cd048e2ca6e2a1d39
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-19 13:06:45 +0530

    Updated method to verify JWT Token

--- server/Utitlities/JWTHandlers/verifyToken.js
+++ server/Utitlities/JWTHandlers/verifyToken.js
@@ -1,1 +1,15 @@
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+const verifyToken = async (token) => {
+  try {
+    const decodedToken = await promisify(jwt.verify)(
+      token,
+      process.env.JWT_SECRET
+    );
+    return decodedToken;
+  } catch (error) {
+    return error;
+  }
+};
+module.exports = verifyToken;
