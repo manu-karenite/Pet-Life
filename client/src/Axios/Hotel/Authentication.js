7713d691b39c0064faa3d8dcc5ba6820e77f7ed5
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-02-19 13:11:39 +0530

    Created AXIOS fnctions to call API at Backend

--- client/src/Axios/Hotel/Authentication.js
+++ client/src/Axios/Hotel/Authentication.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const registerHotel = async (data) => {
-  console.log(`${process.env.REACT_APP_BACKEND_URL}/hotel/register-hotel`);
   const result = await axios({
     method: "POST",
     url: `${process.env.REACT_APP_BACKEND_URL}/hotel/register-hotel`,
@@ -9,4 +8,12 @@ const registerHotel = async (data) => {
   });
   return result;
 };
-export { registerHotel };
+const registerHotelConfirm = async (data) => {
+  const result = await axios({
+    method: "POST",
+    url: `${process.env.REACT_APP_BACKEND_URL}/hotel/register-hotel/confirm`,
+    data: data,
+  });
+  return result;
+};
+export { registerHotel, registerHotelConfirm };
