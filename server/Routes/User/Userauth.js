5d306cef499f74c4f4306b995808a84459ef0e8b
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-03-31 14:13:18 +0530

    Contact Us Page Controller Page Commit:

--- server/Routes/User/Userauth.js
+++ server/Routes/User/Userauth.js
@@ -9,16 +9,18 @@ const {
   forgotPassword,
   verifyOTP,
   updatePassword,
+  contactUs,
 } = require("../../Controllers/User/Authentication.js");
 
 AuthUserRouter.route("/register-user").post(registerUser);
-AuthUserRouter.route("/register-user/confirm").post(
-  registerUserConfirm
-);
+AuthUserRouter.route("/register-user/confirm").post(registerUserConfirm);
 AuthUserRouter.route("/login").post(loginUser);
 AuthUserRouter.route("/verify-user").get(verifyUser);
 AuthUserRouter.route("/forgot-password").post(forgotPassword);
 AuthUserRouter.route("/verify-otp").post(verifyOTP);
 AuthUserRouter.route("/update-password").patch(updatePassword);
+
+//to Contact the Admin
+AuthUserRouter.route("/contact-us").post(contactUs);
 const object = { AuthUserRouter };
 module.exports = object;
